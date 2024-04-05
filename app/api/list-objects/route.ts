aecf4de017eb7fdad1a271590a548c35c85b9f5c
Chris Bonifacio <christopher.bonifacio@gmail.com>
2024-04-05 15:57:33 -0400

    fix bucket variable name

--- app/api/list-objects/route.ts
+++ app/api/list-objects/route.ts
@@ -4,7 +4,7 @@ import { getRequestContext } from "@cloudflare/next-on-pages";
 export const runtime = "edge";
 
 export async function GET(request: NextRequest) {
-  const bucket = getRequestContext().env.MY_BUCKET;
+  const bucket = getRequestContext().env.AMPLIFY_BUCKET;
 
   const objects = (await bucket.list()).objects;
 
